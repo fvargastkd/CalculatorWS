9cc3fec1db5764b0ebb7a8ad160238667f06fa67
fabio_vargas <fabio_vargas@sicredi.com.br>
2020-03-13 16:14:49 -0300

    Ajustes

---
+++ CalculatorClientWSO2/src/main/java/org/me/calculatorclientwso2/CalculatorClientWSO2Service.java
@@ -0,0 +1,70 @@
+package org.me.calculatorclientwso2;
+
+import org.me.dto.AccessToken;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.xml.namespace.QName;
+import javax.xml.ws.BindingProvider;
+import javax.xml.ws.Service;
+import javax.xml.ws.handler.MessageContext;
+import org.apache.commons.codec.binary.Base64;
+import org.me.calculator.CalculatorWS;
+import org.me.calculator.CalculatorWSService;
+
+/**
+ *
+ * @author fabio_vargas
+ */
+@WebService(serviceName = "CalculatorClientWSO2Service")
+public class CalculatorClientWSO2Service {
+
+    @WebMethod(operationName = "add")
+    public int add(@WebParam(name = "i") int i, @WebParam(name = "j") int j) throws Exception {
+        URL url = new URL("http://lt9c1dyr2:8080/App/CalculatorWSService?wsdl");
+        QName qname = new QName("http://calculator.me.org/", "CalculatorWSService");
+        Service service = Service.create(url, qname);
+        CalculatorWS calculator = service.getPort(CalculatorWS.class);
+        return calculator.add(i, j);
+    }
+
+    @WebMethod(operationName = "addWSO2")
+    public int addWSO2(@WebParam(name = "i") int i, @WebParam(name = "j") int j) throws Exception {
+        Service service = new CalculatorWSService();
+        CalculatorWS calculatorService = applySecurityAccess(service);
+        return calculatorService.add(i, j);
+    }
+
+    public static CalculatorWS applySecurityAccess(Service service) throws Exception{
+        
+        String userName = "orjivDl4s_W9ircyB9wCUIJ9aXEa";
+        String passWord = "r0WkOBNhmg4T4L4nYuwgeXaNu1Ua";
+        Token token = new Token();
+        AccessToken acess = token.getToken(userName, passWord);
+        String accessToken = acess.getAccessToken();
+        CalculatorWS port = service.getPort(CalculatorWS.class);
+        Map<String, List<String>> headers = new HashMap<String, List<String>>();
+        headers.put("Authorization", Arrays.asList("Bearer " + accessToken));
+        BindingProvider bp = (BindingProvider) port;
+        bp.getRequestContext().put(MessageContext.HTTP_REQUEST_HEADERS, headers);
+        bp.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, "http://lt9c1dyr2:8280/CalculatorWSService/1.0");
+        return port;
+    }
+
+    public static String encodeCredentials(String username, String password) {
+        String cred = username + ":" + password;
+        String encodedValue = null;
+        byte[] encodedBytes = Base64.encodeBase64(cred.getBytes());
+        encodedValue = new String(encodedBytes);
+        System.out.println("encodedBytes " + new String(encodedBytes));
+        byte[] decodedBytes = Base64.decodeBase64(encodedBytes);
+        System.out.println("decodedBytes " + new String(decodedBytes));
+        return encodedValue;
+    }
+
+}
---
+++ CalculatorClientWSO2/src/main/java/org/me/oauth/TrustCertificate.java
@@ -0,0 +1,86 @@
+package org.me.oauth;
+
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+public class TrustCertificate {
+
+    private static SSLSocketFactory sslSocketTrust = null;
+    private static HostnameVerifier hostVerifierTrust = null;
+    private static SSLContext sslContextTrust = null;
+
+    public static SSLContext getSSLContext() {
+        try {
+            getSslSocketTrust();
+        } catch (KeyManagementException | NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+        return sslContextTrust;
+    }
+
+    private static SSLSocketFactory getSslSocketTrust() throws NoSuchAlgorithmException, KeyManagementException {
+        if (TrustCertificate.sslSocketTrust == null) {
+            final TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+
+                public void checkClientTrusted(final java.security.cert.X509Certificate[] arg0, final String arg1) throws CertificateException {
+                }
+
+                public void checkServerTrusted(final java.security.cert.X509Certificate[] arg0, final String arg1) throws CertificateException {
+                }
+            }};
+            sslContextTrust = SSLContext.getInstance("SSL");
+            sslContextTrust.init(null, trustAllCerts, new java.security.SecureRandom());
+            TrustCertificate.sslSocketTrust = sslContextTrust.getSocketFactory();
+        }
+        return TrustCertificate.sslSocketTrust;
+    }
+
+    public static void allowAllCerts() {
+        final SSLSocketFactory sslSocket = HttpsURLConnection.getDefaultSSLSocketFactory();
+        try {
+            if (sslSocket != TrustCertificate.getSslSocketTrust()) {
+                HttpsURLConnection.setDefaultSSLSocketFactory(TrustCertificate.getSslSocketTrust());
+                TrustCertificate.allowHosts();
+            }
+        } catch (final KeyManagementException e) {
+            throw new RuntimeException(e);
+        } catch (final NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * @return the hostVerifierTrust
+     */
+    private static HostnameVerifier getHostVerifierTrust() {
+        if (TrustCertificate.hostVerifierTrust == null) {
+            TrustCertificate.hostVerifierTrust = new HostnameVerifier() {
+                public boolean verify(final String hostname, final SSLSession session) {
+                    return true;
+                }
+            };
+
+        }
+        return TrustCertificate.hostVerifierTrust;
+    }
+
+    private static void allowHosts() {
+        final HostnameVerifier hostnameVerifier = HttpsURLConnection.getDefaultHostnameVerifier();
+        if (hostnameVerifier != TrustCertificate.getHostVerifierTrust()) {
+            HttpsURLConnection.setDefaultHostnameVerifier(TrustCertificate.getHostVerifierTrust());
+        }
+    }
+
+}
